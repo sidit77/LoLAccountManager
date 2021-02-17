afdf5ed63a23632d9701dfae0d5f79f0e357d553
sidit77 <sidit77@gmail.com>
2021-02-17 13:24:49 +0100

    Installed SharpZipLib for new db format

--- LoLPasswordManager/Program.cs
+++ LoLPasswordManager/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,9 +16,39 @@ namespace LoLPasswordManager
     [STAThread]
     static void Main()
     {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new mainForm());
-    }
+
+            using (var fs = File.Create("html-archive.zip"))
+            {
+                using (var outStream = new ZipOutputStream(fs))
+                {
+                    outStream.Password = "Hallo";
+                    outStream.PutNextEntry(new ZipEntry("content.txt"));
+                    using (var sw = new StreamWriter(outStream))
+                    {
+                        sw.Write("Hello World");
+                    }
+                }
+            }
+
+
+            using (var fs = File.OpenRead("html-archive.zip"))
+            {
+                using (var zf = new ZipFile(fs))
+                {
+                    zf.Password = "Hallo";
+                    using (var zipStream = zf.GetInputStream(zf.GetEntry("content.txt")))
+                    {
+                        using (var sr = new StreamReader(zipStream))
+                        {
+                            MessageBox.Show(sr.ReadToEnd());
+                        }
+                    }
+                }
+            }
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new mainForm());
+        }
   }
 }
