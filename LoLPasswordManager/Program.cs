4cf35d5320c5b7747a198697c82587b4ce712172
sidit77 <sidit77@gmail.com>
2021-11-04 13:29:18 +0100

    fixed platform warning

--- LoLPasswordManager/Program.cs
+++ LoLPasswordManager/Program.cs
@@ -4,17 +4,17 @@ using System.Windows.Forms;
 namespace LoLPasswordManager
 {
     static class Program
-  {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
     {
-      Application.EnableVisualStyles();
-      //Application.SetHighDpiMode(HighDpiMode.SystemAware);
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new mainForm());
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new mainForm());
+        }
     }
-  }
 }
